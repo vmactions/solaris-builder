6ea135fdf550d0004807fafe0375d0fbac92f2fa
neil <win10@neilpang.com>
2020-10-19 21:58:24 +0800

    fix text

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
+echo "1\ny" | pkgadd -v -d http://get.opencsw.org/now
+cp .bashrc .bash_profile
+gsed -i 's|#SUPATH=/usr/bin:/usr/sbin|SUPATH=/usr/bin:/usr/sbin:/opt/csw/bin|' /etc/default/login
 
 
 
-echo "1\ny" |  pkgadd -v   -d http://get.opencsw.org/now
 
-cp  .bashrc  .bash_profile
 
-gsed -i 's|#SUPATH=/usr/bin:/usr/sbin|SUPATH=/usr/bin:/usr/sbin:/opt/csw/bin|'  /etc/default/login
 
 
