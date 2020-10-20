9a24cc8e7030ce6d640a2a366afd8a0aedfa4b0f
neil <github@neilpang.com>
2020-10-20 09:43:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-echo "1\ny" | pkgadd -v -d http://get.opencsw.org/now
+echo y |  pkgadd -v  -d http://get.opencsw.org/now  all
 cp .bashrc .bash_profile
 gsed -i 's|#SUPATH=/usr/bin:/usr/sbin|SUPATH=/usr/bin:/usr/sbin:/opt/csw/bin|' /etc/default/login
 
