26280ad34578291e9b6f2a168a8b89d324218a4f
neil <neilgit@neilpang.com>
2023-11-10 19:50:36 +0100

    use libvirt

--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ fi
 chmod +x "$vmsh"
 
 
-$vmsh addSSHHost  $osname $sshport
+
 
 
 
@@ -70,22 +70,13 @@ if ! $vmsh clearVM $osname; then
   echo "vm does not exists"
 fi
 
-$vmsh createVM  $VM_ISO_LINK $osname $ostype $sshport
-
-
 
 $vmsh startWeb $osname
 
 
 
-$vmsh startCF
+$vmsh createVM  $VM_ISO_LINK $osname $ostype $sshport
 
-
-_sleep=20
-echo "Sleep $_sleep seconds, please open the link in your browser."
-sleep $_sleep
-
-$vmsh startVM $osname
 
 sleep 2
 
@@ -97,9 +88,9 @@ $vmsh  processOpts  $osname  "$opts"
 $vmsh shutdownVM $osname
 
 
-$vmsh detachISO $osname
 
 $vmsh startVM $osname
+
 
 
 
@@ -118,13 +109,17 @@ inputKeys "string root; enter; sleep 1; string $VM_ROOT_PASSWORD ; enter"
 sleep 2
 
 
+if [ ! -e ~/.ssh/id_rsa ] ; then 
+  ssh-keygen -f  ~/.ssh/id_rsa -q -N "" 
+fi
+
 echo "set -e" >enablessh.local
 echo "mkdir -p ~/.ssh" >>enablessh.local
 echo "touch ~/.ssh/authorized_keys" >>enablessh.local
 
 
 #add ssh key twice, to avoid bugs.
-echo "echo '$(base64 ~/.ssh/id_rsa.pub)' | openssl base64 -d >>~/.ssh/authorized_keys" >>enablessh.local
+echo "echo '$(base64 -w 0 ~/.ssh/id_rsa.pub)' | openssl base64 -d >>~/.ssh/authorized_keys" >>enablessh.local
 echo "" >>enablessh.local
 
 echo "echo '$(cat ~/.ssh/id_rsa.pub)' >>~/.ssh/authorized_keys" >>enablessh.local
@@ -138,18 +133,21 @@ cat enablessh.txt >>enablessh.local
 
 $vmsh inputFile $osname enablessh.local
 
+
+###############################################################
+
+$vmsh addSSHHost  $osname
+
+
 ssh $osname sh <<EOF
 echo 'StrictHostKeyChecking=accept-new' >.ssh/config
 
 echo "Host host" >>.ssh/config
-echo "     HostName  10.0.2.2" >>.ssh/config
+echo "     HostName  192.168.122.1" >>.ssh/config
 echo "     User runner" >>.ssh/config
 echo "     ServerAliveInterval 1" >>.ssh/config
 
 EOF
-
-
-###############################################################
 
 
 if [ -e "hooks/postBuild.sh" ]; then
@@ -179,13 +177,13 @@ $vmsh shutdownVM $osname
 
 
 
-ova="$osname-$VM_RELEASE.ova"
+ova="$osname-$VM_RELEASE.qcow2"
 
 
 echo "Exporting $ova"
 $vmsh exportOVA $osname "$ova"
 
-cp ~/.ssh/id_rsa  $osname-$VM_RELEASE-mac.id_rsa
+cp ~/.ssh/id_rsa  $osname-$VM_RELEASE-host.id_rsa
 
 
 ls -lah
