52ae16855d8ff50e89879998be5eba2a6af0a85b
neil <gitpc@neilpang.com>
2025-01-19 19:20:16 +0100

    remove root password

--- hooks/finalize.sh
+++ hooks/finalize.sh
@@ -1,3 +1,12 @@
+
+
+
+sed 's/^PASSREQ=YES/PASSREQ=NO/' /etc/default/login > /tmp/login.new
+cat /tmp/login.new >/etc/default/login
+passwd -d root
+rm -f /tmp/login.new
+
+
 echo "Purging any stale OS snapshots..."
 beadm list | tail +3 | while read -r line; do
   name=`echo $line | awk '{ print $1 };'`
